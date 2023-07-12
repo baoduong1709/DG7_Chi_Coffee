3ea4fc444a1b45db98304d5c1563c74959231933
Duong Huy Bao <201121521103@due.udn.vn>
2023-07-12 16:51:35 +0700

    12/07/2023

---
+++ src/config/db/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+async function connect() {
+    try {
+        await mongoose.connect('mongodb+srv://bao1709:bao17092002@cluster0.ni9fgfo.mongodb.net/dg7_employee', {
+
+        })
+        console.log('Connected!')
+    } catch (error) {
+        console.log('fail!')
+    }
+}
+module.exports = { connect }
+
+// mongodb+srv://bao1709:bao17092002@cluster0.ni9fgfo.mongodb.net/?retryWrites=true&w=majority
