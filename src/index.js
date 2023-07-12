8163ac5e1cb094798b369c07e086cf7aba913349
Duong Huy Bao <201121521103@due.udn.vn>
2023-07-12 16:58:07 +0700

    12/07/2023

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+const express = require('express')
+const route = require('./routes')
+const app = express()
+const port = 5000
+const db = require('./config/db')
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
+db.connect()
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
+route(app)
+
+app.listen(port, () =>
+    console.log(`App listening at http://localhost:${port}`),
+);
