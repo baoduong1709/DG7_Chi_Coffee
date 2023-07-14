953c6ca46640faa207795763297c02e21e7ed933
Duong Huy Bao <201121521103@due.udn.vn>
2023-07-14 10:37:21 +0700

    update link database

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,9 +5,9 @@ async function connect() {
         await mongoose.connect("mongodb+srv://bao1709:bao17092002@cluster0.ni9fgfo.mongodb.net/DG7_Chi_Coffee?retryWrites=true&w=majority", {
 
         })
-        console.log('Connected!')
+        console.log('Connected to MongoDB')
     } catch (error) {
-        console.log('fail!')
+        console.log('Can not connect to MongoDB')
     }
 }
 module.exports = { connect }
