30092b2379c9f9c73cc91e2bd88bd41331562723
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-07-17 15:05:37 +0700

    17/07/2023

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 
-const viewProductRouter = require('./products/ViewProductRoute')
+const viewAllProductRouter = require('./products/ViewAllProductRoute')
 const createProductRouter = require('./products/CreateProductRoute')
 
 const uploadCloud = require('../config/cloudinary');
 function route(app) {
-        app.use('/api/v1/product/view', viewProductRouter)
+        app.use('/api/v1/product/view', viewAllProductRouter)
         app.use('/api/v1/product/create',uploadCloud.single('product_image'), createProductRouter)
 }
 module.exports = route;
