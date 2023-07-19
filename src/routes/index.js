fee4b221ecb12e7ebb55980e40867478f08898ed
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-07-19 16:15:21 +0700

    d

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
-const viewProductDetailsRouter = require('./products/ViewProductDetailsRoute')
+
 const viewAllProductRouter = require('./products/ViewAllProductRoute')
 function route(app) {
-        app.use('/api/v1/product/view', viewAllProductRouter)
-        app.use('/api/v1/product/details', viewProductDetailsRouter)
+        app.use('/api/v1/product', viewAllProductRouter)
 }
 module.exports = route;
