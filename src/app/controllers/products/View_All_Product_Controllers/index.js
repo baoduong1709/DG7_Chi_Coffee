350bf2ce7226261b5b084f6f834ce584f4a66fcb
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-07-19 14:13:54 +0700

    19/07

--- src/app/controllers/products/View_All_Product_Controllers/index.js
+++ src/app/controllers/products/View_All_Product_Controllers/index.js
@@ -3,8 +3,8 @@ class ViewProductControllers {
     
     view(req, res,next) {
         Product.find({})
-        .then(employees => {
-            res.status(200).json(employees)
+        .then(products => {
+            res.status(200).json(products)
         })
         .catch(err => {
             res.status(500).json({message: 'Some errors occurred while retrieving accounts!'})
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 
 const viewAllProductRouter = require('./products/ViewAllProductRoute')
 const createProductRouter = require('./products/CreateProductRoute')
+const viewAllProductTypeRouter =  require('./product_types/ViewAllProductTypeRoute')
 
 const uploadCloud = require('../config/cloudinary');
 function route(app) {
         app.use('/api/v1/product/view', viewAllProductRouter)
         app.use('/api/v1/product/create',uploadCloud.single('product_image'), createProductRouter)
+        app.use('/api/v1/product-type/view', viewAllProductTypeRouter)
 }
 module.exports = route;
