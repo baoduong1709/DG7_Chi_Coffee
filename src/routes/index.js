c1a3dfb5f149c938ac07b4b0ce9097c0eac70f78
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-07-20 17:12:32 +0700

    Create Product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 
 const employeeRouter = require('./employeeRoute')
+const productRouter = require('./productRoute')
 function route(app) {
     app.use('/api/v1/employee',employeeRouter)
+    app.use('/api/v1/product', productRouter)
 }
 module.exports = route;
