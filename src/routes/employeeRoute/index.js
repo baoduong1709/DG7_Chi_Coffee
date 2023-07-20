797f0634548e1ccb5755cd4c3a74c2fae3fa947f
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-07-20 15:05:28 +0700

    1

--- src/routes/employeeRoute/index.js
+++ src/routes/employeeRoute/index.js
@@ -6,4 +6,6 @@ const CheckAdmin = require('../../app/security/CheckAdmin')
 const UpdateEmployeeControllers = require('../../app/controllers/employees/Update_Employee_Controllers')
 router.put('/:id/update',CheckLogin.check, CheckAdmin.check, UpdateEmployeeControllers.update)
 
+const DeleteEmployeeControllers = require('../../app/controllers/employees/Delete_Employee_Controllers')
+router.delete('/:id/delete',CheckLogin.check, CheckAdmin.check, DeleteEmployeeControllers.delete)
 module.exports = router;
