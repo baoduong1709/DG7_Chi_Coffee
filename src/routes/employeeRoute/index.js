4a7f95e280c988d0673356bc230d611951965985
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-07-21 11:09:52 +0700

    View Employee Details

--- src/routes/employeeRoute/index.js
+++ src/routes/employeeRoute/index.js
@@ -6,6 +6,9 @@ const CheckAdmin = require('../../app/security/CheckAdmin')
 const ViewEmployeeListControllers = require('../../app/controllers/employees/View_Employee_List_Controllers')
 router.get('/', ViewEmployeeListControllers.view)
 
+const ViewEmployeeDetailsControllers = require('../../app/controllers/employees/View_Employee_Details_Controllers')
+router.get('/:id', ViewEmployeeDetailsControllers.view)
+
 const LoginEmployeeControllers = require('../../app/controllers/employees/Login_Employee_Controllers')
 router.post('/auth', LoginEmployeeControllers.login)
 
