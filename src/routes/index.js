52aab9e1741425e1c7ad6f0a99c54cd84b8092c6
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-07-21 10:37:42 +0700

    Login Customer

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-
+const customerRouter = require('./customerRoute')
 const employeeRouter = require('./employeeRoute')
 const productRouter = require('./productRoute')
 const productTypeRouter = require('./productTypeRoute')
@@ -6,5 +6,6 @@ function route(app) {
     app.use('/api/v1/employee',employeeRouter)
     app.use('/api/v1/product', productRouter)
     app.use('/api/v1/product-type',productTypeRouter)
+    app.use('/api/v1/customer', customerRouter)
 }
 module.exports = route;
