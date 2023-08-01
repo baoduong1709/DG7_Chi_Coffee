08c567466de43b3781b779b90386230e6d55eadd
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-08-01 09:01:04 +0700

    Fix

--- src/app/models/Order/index.js
+++ src/app/models/Order/index.js
@@ -2,10 +2,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const Order = new Schema({
-    customer_id: String,
-    customer_name: String,
-    employee_id: String,
-    employee_name: String,
+    customer_id: {
+        type: String,
+        required: true,
+        immutable: true
+    },
+    customer_name: {
+        type: String,
+        required: true,
+        immutable: true
+    },
+    employee_id: {
+        type: String,
+    },
+    employee_name: {
+        type: String,
+    },
     table_id: String,
     product:[{
         product_id: String,
