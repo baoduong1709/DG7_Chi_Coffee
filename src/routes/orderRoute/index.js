41b1e0b8390fd1c2ecff92aeed47e5b293b06ff7
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-08-01 16:25:33 +0700

    v

--- src/routes/orderRoute/index.js
+++ src/routes/orderRoute/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const CheckLogin =require('../../app/security/CheckLoginEmployee')
+const CheckLoginEmployee =require('../../app/security/CheckLoginEmployee')
+const CheckLoginCustomer = require('../../app/security/CheckLoginCustomer')
 
-const CreateOrderOnlineController = require('../../app/controllers/orders/Create_Order_Controllers')
-router.post('/create', CreateOrderOnlineController.create)
+const CreateOrderController = require('../../app/controllers/orders/Create_Order_Controllers')
+router.post('/create',CheckLoginCustomer.check, CreateOrderController.create)
 
 const UpdateOrderOnlineController = require('../../app/controllers/orders/Update_Order_Controllers')
-router.put('/:_id/update',CheckLogin.check, UpdateOrderOnlineController.update)
+router.put('/:_id/update',CheckLoginEmployee.check, UpdateOrderOnlineController.update)
 
 const ViewOrderHistoryController = require('../../app/controllers/orders/View_Order_History_Controllers')
-router.get('/history', ViewOrderHistoryController.view)
+router.get('/history',CheckLoginCustomer.check, ViewOrderHistoryController.view)
 
 module.exports = router
