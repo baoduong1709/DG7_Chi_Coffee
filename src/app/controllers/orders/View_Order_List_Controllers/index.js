32dea010288fb9db5d905114d8a4365be12b20f8
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-08-04 15:17:11 +0700

    Update index.js

--- src/app/controllers/orders/View_Order_List_Controllers/index.js
+++ src/app/controllers/orders/View_Order_List_Controllers/index.js
@@ -3,7 +3,10 @@ const Order = require('../../../models/Order')
 class ViewOrderListControllers {
     
     async view(req, res, next) {
-        await Order.find()
+        const status = req.query.status
+        console.log(status)
+        if (status == undefined) {
+            Order.find()
             .then(order => {
                 if(order == '') {
                     return res.status(404).send('Chưa có hóa đơn')
@@ -13,6 +16,19 @@ class ViewOrderListControllers {
             .catch(err => {
                 res.status(500).send('Lỗi server')
             })
+        }else{
+            Order.find({status:status})
+            .then(order => {
+                if(order == '') {
+                    return res.status(404).send('Chưa có hóa đơn')
+                }
+                res.status(200).json(order)
+            })
+            .catch(err => {
+                res.status(500).send('Lỗi server')
+            })
+        }
+
     };
 }
 module.exports = new ViewOrderListControllers;
