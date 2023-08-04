9abb8820fc754c32c1ee73afc2fb7975cd08a037
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-08-04 15:51:44 +0700

    Update index.js

--- src/app/controllers/orders/View_Order_List_Controllers/index.js
+++ src/app/controllers/orders/View_Order_List_Controllers/index.js
@@ -3,32 +3,21 @@ const Order = require('../../../models/Order')
 class ViewOrderListControllers {
     
     async view(req, res, next) {
-        const status = req.query.status
-        console.log(status)
-        if (status == undefined) {
-            Order.find()
-            .then(order => {
-                if(order == '') {
-                    return res.status(404).send('Chưa có hóa đơn')
-                }
-                res.status(200).json(order)
-            })
-            .catch(err => {
-                res.status(500).send('Lỗi server')
-            })
-        }else{
-            Order.find({status:status})
-            .then(order => {
-                if(order == '') {
-                    return res.status(404).send('Chưa có hóa đơn')
-                }
-                res.status(200).json(order)
-            })
-            .catch(err => {
-                res.status(500).send('Lỗi server')
-            })
+        const status = req.query.status;
+        try {
+            let orders; 
+            if (status === undefined) {
+                orders = await Order.find();
+            } else {
+                orders = await Order.find({ status: status });
+            }    
+            if (orders.length === 0) {
+                return res.status(404).send('Chưa có hóa đơn');
+            }   
+            res.status(200).json(orders);
+        } catch (err) {
+            res.status(500).send('Lỗi server');
         }
-
     };
 }
 module.exports = new ViewOrderListControllers;
