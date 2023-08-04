99a8e1641ed38d31976fe32f85ee3d20581e2e10
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-08-04 11:38:31 +0700

    Update index.js

--- src/routes/dashboardRoute/index.js
+++ src/routes/dashboardRoute/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 const CheckLoginEmployee =require('../../app/security/CheckLoginEmployee')
 
 const View_Chart_Controllers = require('../../app/controllers/dashboard/View_Chart_Controllers')
-router.get('/chart',CheckLoginEmployee.check,View_Chart_Controllers.view)
+router.get('/revenue',CheckLoginEmployee.check,View_Chart_Controllers.view)
 
 const View_Statistical_Controllers = require('../../app/controllers/dashboard/View_Statistical_Controllers')
 router.get('/statistical',CheckLoginEmployee.check,View_Statistical_Controllers.view)
