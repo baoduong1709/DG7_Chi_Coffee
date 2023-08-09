95d144f08323bea871318b1405c01b3c12063d43
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-08-09 16:06:19 +0700

    x

--- src/app/controllers/table/Update_Table_Controllers/index.js
+++ src/app/controllers/table/Update_Table_Controllers/index.js
@@ -2,6 +2,18 @@ const Table = require('../../../models/Table')
 class UpdateTableControllers {
     update(req, res, next) {
         let _id = req.body._id
+        console.log(_id)
+        Table.findByIdAndUpdate(_id,{
+            status: false,
+            order_id: ''
+        })
+        .then((data)=>{
+            res.status(200).send('Cập nhật thành công')
+        })
+        .catch(err =>{
+            res.status(500).send('Lỗi server')
+        })
+
     }
 }
 module.exports = new UpdateTableControllers
--- src/routes/tableRoute/index.js
+++ src/routes/tableRoute/index.js
@@ -6,4 +6,7 @@ const CheckLoginCustomer = require('../../app/security/CheckLoginCustomer')
 const ViewTableController = require('../../app/controllers/table/View_Table_Controllers')
 router.get('/',CheckLoginEmployee.check, ViewTableController.view)
 
+const UpdateTableController = require('../../app/controllers/table/Update_Table_Controllers')
+router.put('/update',CheckLoginEmployee.check, UpdateTableController.update)
+
 module.exports = router
