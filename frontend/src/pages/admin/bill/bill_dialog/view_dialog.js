3673aebd82ac380239c9e4d094a72ea443bd3759
baoduong1709 <110188480+baoduong1709@users.noreply.github.com>
2023-08-11 11:37:13 +0700

    Revert "Merge pull request #98 from baoduong1709/Dev_Q3"
    This reverts commit 238c42e7206e1fa4067e90e6b817c5718e1b5d37, reversing
    changes made to 7c437bff434711afe41477e136aec22b9bd93264.

--- frontend/src/pages/admin/bill/bill_dialog/view_dialog.js
+++ frontend/src/pages/admin/bill/bill_dialog/view_dialog.js
@@ -3,33 +3,9 @@ import { DialogActions, DialogContent, DialogTitle, TextField, Button, Grid, Typ
 import { NumericFormatCustom, StyledDialog } from '~/components/private_layout/theme';
 import dayjs from 'dayjs';
 import { NumericFormat } from 'react-number-format';
-import { customerAPI } from '~/api/customer';
-import { ToastOption } from '~/components';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 
 export const FormDialog = ({ isDialogOpened, item, handleCloseDialog }) => {
     const [rows, setRows] = React.useState([]);
-    const [customer, setCustomer] = React.useState({});
-    React.useEffect(() => {
-        if(item.table_id == undefined) {
-            const customer_id = item.customer_id? item.customer_id: null;
-            const getCustomerDetail = async () => {
-                try {
-                    const res = await customerAPI.get(customer_id);
-                    setCustomer(res);
-                } catch (err) {
-                    let status = err.status;
-                    let data = err.data;
-                    toast.error('Lỗi ' + status + ': ' + data, ToastOption);
-                }
-            }
-            if(customer_id) {
-                getCustomerDetail();
-            }
-        }
-    }, [item])
     React.useEffect(() => {
         setRows(item.product);
     }, [item])
@@ -57,15 +33,6 @@ export const FormDialog = ({ isDialogOpened, item, handleCloseDialog }) => {
                             <Typography variant="subtitle1">{"Xác nhận lúc:  "}</Typography>
                             <Typography variant="subtitle1" marginLeft={4.2}>{item.updatedAt? dayjs(item.updatedAt).format("HH:mm:ss DD/MM/YYYY").toString(): ""}</Typography>
                         </Grid>
-                        {item.table_id? 
-                        <Grid item container direction="row">
-                            <Typography variant="subtitle1">{"Bàn:  "}</Typography>
-                            <Typography variant="subtitle1" marginLeft={11.5}>{item.table_id}</Typography>
-                        </Grid>
-                        : <Grid item container direction="row">
-                            <Typography variant="subtitle1">{"Địa chỉ giao:  "}</Typography>
-                            <Typography variant="subtitle1" marginLeft={5}>{customer.address}</Typography>
-                        </Grid>}
                         <Grid item container direction="row">
                             <TableContainer>
                                 <Table aria-label='products-table'>
@@ -106,7 +73,6 @@ export const FormDialog = ({ isDialogOpened, item, handleCloseDialog }) => {
                     <Button onClick={handleCloseDialog}>Đóng lại</Button>
                 </DialogActions>
             </StyledDialog>
-            <ToastContainer />
         </React.Fragment>
     );
 }
